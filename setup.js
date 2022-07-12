610bd60b65563efbd74c48af8e43215c0169bee0
Jack Burgess <jackburgess124@gmail.com>
2022-07-12 18:09:45 +0100

    feat: run in action directory

--- setup.js
+++ setup.js
@@ -1,4 +1,4 @@
 const { execSync } = require('child_process')
 
 console.log({__filename, __dirname})
-execSync('npm install --omit=dev', { cwd: process.cwd() })
+execSync('npm install --omit=dev', { cwd: __dirname })
